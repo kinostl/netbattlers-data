b219987212cedb208a8ffda6cadf3c1110cf60e6
Minty <kinostl@gmail.com>
2023-07-01 23:35:51 -0400

    Make it easier to work with Content Collections

--- chips/generate_chips.sh
+++ chips/generate_chips.sh
@@ -2,6 +2,7 @@
 wget -O chips.csv "https://docs.google.com/spreadsheets/d/1p9SII_sEwtJMgT0F86ZadNENNoGZGsdRjmjYOwEcFEI/gviz/tq?tqx=out:csv&sheet=ChipsU"
 
 JS_OUT=`sqlite3 < generate_chips.sql`
+rm -rf dist/
 mkdir -p dist/json/
 mkdir -p dist/yaml/
 
@@ -10,7 +11,6 @@ IFS='
 for item in $JS_OUT
 do
     my_fbase=`echo $item | jq .kind -r`
-    my_fbase=${my_fbase,,}
     my_fname="dist/json/$my_fbase.json"
     IFS=' '
     echo $item | jq > $my_fname
--- viruses/generate_virus.sh
+++ viruses/generate_virus.sh
@@ -2,6 +2,7 @@
 wget -O viruses.csv "https://docs.google.com/spreadsheets/d/1p9SII_sEwtJMgT0F86ZadNENNoGZGsdRjmjYOwEcFEI/gviz/tq?tqx=out:csv&sheet=Viruses"
 
 JS_OUT=`sqlite3 < generate_virus.sql`
+rm -rf dist/
 mkdir -p dist/json/
 mkdir -p dist/yaml/
 
@@ -10,7 +11,6 @@ IFS='
 for item in $JS_OUT
 do
     my_fbase=`echo $item | jq .kind -r`
-    my_fbase=${my_fbase,,}
     my_fname="dist/json/$my_fbase.json"
     IFS=' '
     echo $item | jq > $my_fname
